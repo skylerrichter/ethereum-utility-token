4feff96c07c67db19a0be1f20151ea92c083e251
Skyler Richter <skylerrichter@gmail.com>
2018-02-27 12:39:30 -0800

    Add basic UI.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import TokenContract from '../build/contracts/Token.json'
 import getWeb3 from './utils/getWeb3'
 
-import './css/oswald.css'
 import './css/open-sans.css'
-import './css/pure-min.css'
 import './App.css'
 
 class App extends Component {
@@ -12,6 +10,12 @@ class App extends Component {
     super(props)
 
     this.state = {
+      contract: null,
+      balance: 0,
+      address: '',
+      from: '',
+      to: '',
+      value: '',
       storageValue: 0,
       web3: null
     }
@@ -44,48 +48,42 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
-
-    // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
+    const token = contract(TokenContract)
+    token.setProvider(this.state.web3.currentProvider)
+    token.deployed().then((contract) => {
+      this.setState({
+        contract
       })
+    })
+
+  }
+
+  getBalance() {
+    this.state.contract.balanceOf(this.state.address).then((balance) => {
+      this.setState({
+        balance: balance.toNumber()
+      })
+    })
+  }
+
+  transfer() {
+    this.state.contract.transfer(this.state.to, this.state.value, {from: this.state.from}).then(() => {
+
     })
   }
 
   render() {
     return (
-      <div className="App">
-        <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
-        </nav>
-
-        <main className="container">
-          <div className="pure-g">
-            <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
-            </div>
-          </div>
-        </main>
+      <div>
+        <h1>Check Balance</h1>
+        <div>Address: <input type="text" onChange={(event) => this.setState({address: event.target.value})}/></div>
+        <div>Balance: {this.state.balance}</div>
+        <div><button onClick={() => this.getBalance()}>Fetch</button></div>
+        <h1>Transfer</h1>
+        <div>From: <input type="text" onChange={(event) => this.setState({from: event.target.value})}/></div>
+        <div>To: <input type="text" onChange={(event) => this.setState({to: event.target.value})}/> </div>
+        <div>Value: <input type="text" onChange={(event) => this.setState({value: event.target.value})}/> </div>
+        <div><button onClick={() => this.transfer()}>Send</button></div>
       </div>
     );
   }
